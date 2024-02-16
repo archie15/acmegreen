24848b9cf5dfb92c0b7a7730c08d05e329581ff6
Archie Cimafranca <chientrisha@gmail.com>
2024-02-16 23:16:03 +0800

    cleaned

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@
 define( 'DB_NAME', 'acmegreen' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'root' ); 
 
 /** MySQL database password */
 define( 'DB_PASSWORD', '' );
